f3ceb015d6e1a7a9b433c16a4885b35d74b379b4
Sam <sams@indeed.com>
2015-07-07 14:09:49 -0500

    code for benchmarking the copied vs uncopied trees

--- real_main.c
+++ real_main.c
@@ -6,11 +6,10 @@
 void bench_tree(size_t num); 
 void bench_mem(size_t num, size_t alloc_size);
 void bench_copy(FILE *outf);
-void bench_small(FILE *outf);
 
 int main() {
     srand(time(NULL));
     FILE *outf = fopen("testout.txt", "w");
-    bench_small(outf);
+    bench_copy(outf);
     fclose(outf);
 }
